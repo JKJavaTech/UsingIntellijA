39450ef7693a45a26dede0ccb2ddfc9a0b6a0266
JKJavaTech <jkjobssearch@gmail.com>
2024-06-13 19:33:53 +0530

    SpringBoot Project

---
+++ SpringBootWithSwagger/src/test/java/com/example/springbootwithswagger/SpringBootWithSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithSwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
